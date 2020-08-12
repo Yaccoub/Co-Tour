2e4384fa0a8472583e30273683a7d52b827fce7a
alaeddine_yacoub <ge35qoz@mytum.de>
2020-08-12 17:10:15 +0200

    changed class

--- Tripadvisor_web_scraper/Preprocessed Datasets/Clustering_prerocessing.py
+++ Tripadvisor_web_scraper/Preprocessed Datasets/Clustering_prerocessing.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 
-def preprocessing(dataframe):
-    df = dataframe.copy()
+def preprocessing(df):
     df['date'] = df['date'].replace({'Date of experience: ': ''}, regex=True)
     df['visit'] = df['visit'].replace({'Trip type: ': ''}, regex=True)
     df['date']= [datetime.strptime(date, '%B %Y')for date in df['date']]
@@ -11,18 +10,34 @@ def preprocessing(dataframe):
 
     return df
 
-def clustering_process(dataframe):
-    df = dataframe.copy()
-    df[['city', 'country', 'extra']] = df['visitor_origin'].str.split(',', expand=True, n=2)
-    #df = pd.get_dummies(df, columns=["visit", "country"])
+def clustering_process(df):
+    df[['city', 'country', 'extra']] = df['visitor_origin'].str.split(', ', expand=True, n=2)
     df = df.drop(['rating','title','text'], axis=1)
     return df
 
-def feature_extraction(dataframe):
-    df = dataframe.copy()
+
+def feature_extraction(df):
     df = preprocessing(df)
     df = clustering_process(df)
-    visitors_by_country = df.groupby('country').count().sort_values('visit', ascending=True)['visitor_origin']
-    type_of_visitors    = df.groupby('visit').count().sort_values('country', ascending=True)['visitor_origin']
-    visitors_by_city    = df.groupby('city').count().sort_values('visit', ascending=True)['visitor_origin']
+    visitors_by_country = df.groupby('country').count().sort_values('visit', ascending=True)
+    type_of_visitors = df.groupby('visit').count().sort_values('country', ascending=True)
+    visitors_by_city = df.groupby('city').count().sort_values('visit', ascending=True)
+
     return visitors_by_country, type_of_visitors, visitors_by_city
+
+def eu_countries(visitors_by_country):
+    visitors_by_country["Non EU"] = 0
+    for i in range (len(visitors_by_country)):
+        if not(visitors_by_country.index[i] in EU_countries):
+            visitors_by_country["Non EU"][i] = int(1)
+    return visitors_by_country
+
+
+def get_visitors(visitors_by_country, visitors_by_city):
+    visitors_from_munich = visitors_by_city['visitor_origin']['Munich']
+    visitors_outside_munich = visitors_by_country['visitor_origin']['Germany'] - visitors_by_city['visitor_origin'][
+        'Munich']
+    visitors_outside_eu = visitors_by_country.groupby('Non EU').sum()['visitor_origin'][1]
+    visitors_from_eu = visitors_by_country.groupby('Non EU').sum()['visitor_origin'][0] - \
+                       visitors_by_country['visitor_origin']['Germany']
+    return visitors_from_munich, visitors_outside_munich, visitors_outside_eu, visitors_from_eu
