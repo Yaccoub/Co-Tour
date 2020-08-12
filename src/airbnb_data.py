5ead3dc84eb182e6c190fca4add8e76d72a18d7a
Stephan Rappensperger <stephan.rappensperger@tum.de>
2020-08-12 11:11:35 +0200

    Changed file path

--- src/airbnb_data.py
+++ src/airbnb_data.py
@@ -3,20 +3,18 @@
 
 # In[131]:
 
-
 # Import the standard libraries
 import pandas as pd
 import numpy as np
+import requests
 
 def download_data(month_listings):
-        
-        
     if month_listings == 'Mar20_listings':
         Airbnb_Mar_listings_url = 'http://data.insideairbnb.com/germany/bv/munich/2020-03-19/visualisations/listings.csv'
         Airbnb_Mar_listings_req = requests.get(Airbnb_Mar_listings_url)
         Airbnb_Mar_listings_url_content = Airbnb_Mar_listings_req.content
         # save the data content in a csv file
-        Airbnb_Mar_listings_csv_file = open('listingsMunichMar20.csv', 'wb')
+        Airbnb_Mar_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichMar20.csv', 'wb')
         Airbnb_Mar_listings_csv_file.write(Airbnb_Mar_listings_url_content)
         Airbnb_Mar_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -27,7 +25,7 @@ def download_data(month_listings):
         Airbnb_Mar_listings_req = requests.get(Airbnb_Mar_listings_url)
         Airbnb_Mar_listings_url_content = Airbnb_Mar_listings_req.content
         # save the data content in a csv file
-        Airbnb_Mar_listings_csv_file = open('listingsMunichMar19.csv', 'wb')
+        Airbnb_Mar_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichMar19.csv', 'wb')
         Airbnb_Mar_listings_csv_file.write(Airbnb_Mar_listings_url_content)
         Airbnb_Mar_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -38,19 +36,18 @@ def download_data(month_listings):
         Airbnb_Apr_listings_req = requests.get(Airbnb_Apr_listings_url)
         Airbnb_Apr_listings_url_content = Airbnb_Apr_listings_req.content
         # save the data content in a csv file
-        Airbnb_Apr_listings_csv_file = open('listingsMunichApr20.csv', 'wb')
+        Airbnb_Apr_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichApr20.csv', 'wb')
         Airbnb_Apr_listings_csv_file.write(Airbnb_Apr_listings_url_content)
         Airbnb_Apr_listings_csv_file.close()
         # read from the dowloaded csv file
         return pd.read_csv('listingsMunichApr20.csv')
-    
-        
+
     elif month_listings == 'Mai20_listings':
         Airbnb_Mai_listings_url = 'http://data.insideairbnb.com/germany/bv/munich/2020-05-24/visualisations/listings.csv'
         Airbnb_Mai_listings_req = requests.get(Airbnb_Mai_listings_url)
         Airbnb_Mai_listings_url_content = Airbnb_Mai_listings_req.content
         # save the data content in a csv file
-        Airbnb_Mai_listings_csv_file = open('listingsMunichMai20.csv', 'wb')
+        Airbnb_Mai_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichMai20.csv', 'wb')
         Airbnb_Mai_listings_csv_file.write(Airbnb_Mai_listings_url_content)
         Airbnb_Mai_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -61,19 +58,18 @@ def download_data(month_listings):
         Airbnb_Mai_listings_req = requests.get(Airbnb_Mai_listings_url)
         Airbnb_Mai_listings_url_content = Airbnb_Mai_listings_req.content
         # save the data content in a csv file
-        Airbnb_Mai_listings_csv_file = open('listingsMunichMai19.csv', 'wb')
+        Airbnb_Mai_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichMai19.csv', 'wb')
         Airbnb_Mai_listings_csv_file.write(Airbnb_Mai_listings_url_content)
         Airbnb_Mai_listings_csv_file.close()
         # read from the dowloaded csv file
         return pd.read_csv('listingsMunichMai19.csv')
-        
-      
+
     elif month_listings == 'June20_listings':
         Airbnb_Jun_listings_url = 'http://data.insideairbnb.com/germany/bv/munich/2020-06-20/visualisations/listings.csv'
         Airbnb_Jun_listings_req = requests.get(Airbnb_Jun_listings_url)
         Airbnb_Jun_listings_url_content = Airbnb_Jun_listings_req.content
         # save the data content in a csv file
-        Airbnb_Jun_listings_csv_file = open('listingsMunichJun20.csv', 'wb')
+        Airbnb_Jun_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichJun20.csv', 'wb')
         Airbnb_Jun_listings_csv_file.write(Airbnb_Jun_listings_url_content)
         Airbnb_Jun_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -84,7 +80,7 @@ def download_data(month_listings):
         Airbnb_Jun_listings_req = requests.get(Airbnb_Jun_listings_url)
         Airbnb_Jun_listings_url_content = Airbnb_Jun_listings_req.content
         # save the data content in a csv file
-        Airbnb_Jun_listings_csv_file = open('listingsMunichJun19.csv', 'wb')
+        Airbnb_Jun_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichJun19.csv', 'wb')
         Airbnb_Jun_listings_csv_file.write(Airbnb_Jun_listings_url_content)
         Airbnb_Jun_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -95,7 +91,7 @@ def download_data(month_listings):
         Airbnb_July_listings_req = requests.get(Airbnb_July_listings_url)
         Airbnb_July_listings_url_content = Airbnb_July_listings_req.content
         # save the data content in a csv file
-        Airbnb_July_listings_csv_file = open('listingsMunichJuly19.csv', 'wb')
+        Airbnb_July_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichJuly19.csv', 'wb')
         Airbnb_July_listings_csv_file.write(Airbnb_July_listings_url_content)
         Airbnb_July_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -106,7 +102,7 @@ def download_data(month_listings):
         Airbnb_Aug_listings_req = requests.get(Airbnb_Aug_listings_url)
         Airbnb_Aug_listings_url_content = Airbnb_Aug_listings_req.content
         # save the data content in a csv file
-        Airbnb_Aug_listings_csv_file = open('listingsMunichAug19.csv', 'wb')
+        Airbnb_Aug_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichAug19.csv', 'wb')
         Airbnb_Aug_listings_csv_file.write(Airbnb_Aug_listings_url_content)
         Airbnb_Aug_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -117,7 +113,7 @@ def download_data(month_listings):
         Airbnb_Sep_listings_req = requests.get(Airbnb_Sep_listings_url)
         Airbnb_Sep_listings_url_content = Airbnb_Sep_listings_req.content
         # save the data content in a csv file
-        Airbnb_Sep_listings_csv_file = open('listingsMunichSep19.csv', 'wb')
+        Airbnb_Sep_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichSep19.csv', 'wb')
         Airbnb_Sep_listings_csv_file.write(Airbnb_Sep_listings_url_content)
         Airbnb_Sep_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -128,19 +124,18 @@ def download_data(month_listings):
         Airbnb_Oct_listings_req = requests.get(Airbnb_Oct_listings_url)
         Airbnb_Oct_listings_url_content = Airbnb_Oct_listings_req.content
         # save the data content in a csv file
-        Airbnb_Oct_listings_csv_file = open('listingsMunichOct19.csv', 'wb')
+        Airbnb_Oct_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichOct19.csv', 'wb')
         Airbnb_Oct_listings_csv_file.write(Airbnb_Oct_listings_url_content)
         Airbnb_Oct_listings_csv_file.close()
         # read from the dowloaded csv file
         return pd.read_csv('listingsMunichOct19.csv')
-    
-    
+
     elif month_listings == 'Nov19_listings':
         Airbnb_Nov_listings_url = 'http://data.insideairbnb.com/germany/bv/munich/2019-11-25/visualisations/listings.csv'
         Airbnb_Nov_listings_req = requests.get(Airbnb_Nov_listings_url)
         Airbnb_Nov_listings_url_content = Airbnb_Nov_listings_req.content
         # save the data content in a csv file
-        Airbnb_Nov_listings_csv_file = open('listingsMunichNov19.csv', 'wb')
+        Airbnb_Nov_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichNov19.csv', 'wb')
         Airbnb_Nov_listings_csv_file.write(Airbnb_Nov_listings_url_content)
         Airbnb_Nov_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -151,7 +146,7 @@ def download_data(month_listings):
         Airbnb_Dec_listings_req = requests.get(Airbnb_Dec_listings_url)
         Airbnb_Dec_listings_url_content = Airbnb_Dec_listings_req.content
         # save the data content in a csv file
-        Airbnb_Dec_listings_csv_file = open('listingsMunichDec19.csv', 'wb')
+        Airbnb_Dec_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichDec19.csv', 'wb')
         Airbnb_Dec_listings_csv_file.write(Airbnb_Dec_listings_url_content)
         Airbnb_Dec_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -162,7 +157,7 @@ def download_data(month_listings):
         Airbnb_Jan_listings_req = requests.get(Airbnb_Jan_listings_url)
         Airbnb_Jan_listings_url_content = Airbnb_Jan_listings_req.content
         # save the data content in a csv file
-        Airbnb_Jan_listings_csv_file = open('listingsMunichJan20.csv', 'wb')
+        Airbnb_Jan_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichJan20.csv', 'wb')
         Airbnb_Jan_listings_csv_file.write(Airbnb_Jan_listings_url_content)
         Airbnb_Jan_listings_csv_file.close()
         # read from the dowloaded csv file
@@ -173,14 +168,13 @@ def download_data(month_listings):
         Airbnb_Feb_listings_req = requests.get(Airbnb_Feb_listings_url)
         Airbnb_Feb_listings_url_content = Airbnb_Feb_listings_req.content
         # save the data content in a csv file
-        Airbnb_Feb_listings_csv_file = open('listingsMunichFeb20.csv', 'wb')
+        Airbnb_Feb_listings_csv_file = open('../data/Airbnb_data/' + 'listingsMunichFeb20.csv', 'wb')
         Airbnb_Feb_listings_csv_file.write(Airbnb_Feb_listings_url_content)
         Airbnb_Feb_listings_csv_file.close()
         # read from the dowloaded csv file
         return pd.read_csv('listingsMunichFeb20.csv')
     else :
         return 'invalid month'
-     
 
 def clean_data(data):
     rec_data = data.loc[:,[ 'neighbourhood','id','number_of_reviews','room_type','minimum_nights','price','last_review']]
@@ -213,19 +207,10 @@ def main():
         ret.append(df)
 
     # Concat the list to one table
-    pd.concat(ret, axis=1, sort=True).T
-    print(ret)
-
-
-
-
+    df = pd.concat(ret, axis=1, sort=True).T
+    df.to_csv('../data/Airbnb_data/listings.csv')
 
 if __name__ == '__main__':
     main()
 
-
 # In[ ]:
-
-
-
-
