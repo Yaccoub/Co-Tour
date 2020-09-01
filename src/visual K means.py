e83e851c255d58aa9aeb2b6b03563b11dd3740e0
Kheireddine Achour <ge38vuf@mytum.de>
2020-09-02 00:32:20 +0200

    Visualisation of K means

--- src/visual K means.py
+++ src/visual K means.py
@@ -1,23 +1,23 @@
+import numpy as np
 import pandas as pd
-import glob
-from geopy.geocoders import Nominatim
 import folium
 
-def visualise(df):
-    # create a map centered on munich
-    map1 = folium.Map(
-        location=[48.137154,11.576124],
-        tiles='cartodbpositron',
-        zoom_start=4,
-        max_zoom=20,
-        min_zoom=12
-    )
-    # add a marker on each country propotional to the number of visitors to the selected location
-    data.apply(lambda row: folium.CircleMarker(
-        location=[row[x], row[y]], color=row["color"],
-        fill=True, tooltip='this is the' + str(row["attraction_name"]), radius=4).add_to(map_), axis=1)
-
-    return map1
-
-
+data = pd.read_csv("../data/K_means_data/clusters.csv")
+geocoordinates = pd.read_csv('../data/geocoordinates/geoattractions.csv')
+data['latitude']= geocoordinates['latitude']
+data['longitude']= geocoordinates['longitude']
+x, y = "latitude", "longitude"
+color = "Cluster"
+lst_elements = sorted(list(data[color].unique()))
+lst_colors = ['#%06X' % np.random.randint(0, 0xFFFFFF) for i in
+              range(len(lst_elements))]
+data["color"] = data[color].apply(lambda x:
+                lst_colors[lst_elements.index(x)])
+# initialize the map with the starting location
+map_ = folium.Map(location=[48.137154,11.576124], tiles="cartodbpositron",
+                  zoom_start=13)## add points
+data.apply(lambda row: folium.CircleMarker(
+    location=[row[x],row[y]], color=row["color"],
+    fill=True, tooltip='this is the' + str(row["attraction_name"]) ,radius=8).add_to(map_), axis=1)
+map_
 
